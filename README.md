b927f14576e7e0bb41bad973295bb86f62b2f393
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-08-06 20:28:41 -0300

    README, estrutura do banco, docker

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bairros Brasileiros
+
+Aplicação para buscar os bairros brasileiros em sites da internet e banco de dados para o armazenamento.
+
+## E o IBGE?
+
+O site do IBGE é um site *hostil* para a busca estruturada de dados de muitas localidades. Ele oferece cidade a cidade, sem nenhuma api visivelmente clara. Além do mais, ele é bem limitado sobre a informação dos bairros.
---
+++ migration.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE bairros_brasileiros;
+
+CREATE TABLE locais (
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY,
+    `local` VARCHAR(255),
+    `parentalidade` INT UNSIGNED UNIQUE,
+    `tipo_localidade` INT UNSIGNED UNIQUE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+ALTER TABLE locais ADD CONSTRAINT `parentalidade_id_constraint` FOREIGN KEY (`parentalidade`) REFERENCES locais (`id`);
+
+CREATE TABLE tipos_locais (
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY,
+    `tipo` VARCHAR(32)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+ALTER TABLE `tipos_locais` ADD CONSTRAINT `tipos_locais_tipo_localidade_constraint` FOREIGN KEY (`id`) REFERENCES `locais` (`tipo_localidade`);
