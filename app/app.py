92f9dc18400c3a7dbaba2fe8b937278e0c07d864
Danilo Silva <contact@danilocgsilva.me>
2024-09-14 20:56:22 -0300

    Banco de dados, conexão, flask

--- app/app.py
+++ app/app.py
@@ -1,21 +1,21 @@
-from utils.Buscar_Arquivos_Aplicacao import Buscar_Arquivos_Aplicacao
-from comandos.Adicionar_Cidade import Adicionar_Cidade
-from comandos.Listar_Cidades import Listar_Cidades
+from flask import Flask
+import os
+import mysql.connector
 
-def mostrar_comandos():
-    buscar_arquivos_aplicacao = Buscar_Arquivos_Aplicacao()
-    comandos_arquivos = buscar_arquivos_aplicacao.buscar_nome_arquivos_comandos()
+app = Flask(__name__)
 
-    print("Comandos disponíveis:")
+@app.route("/")
+def hello():
+    path = os.path.join('migrations', 'migration.sql')
+    file = open(path, "r")
+    query = file.read()
+    
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        user="root",
+        password="tiger",
+        database="<database_name>"
+    )
 
-    objeto_comandos = []
-    for arquivo_comando in comandos_arquivos:
-        nome_classe = arquivo_comando.split(".")[0]
-        class_obj = globals()[nome_classe]
-        objeto_comandos.append(class_obj())
-        
-    for objeto_comando in objeto_comandos:
-        print(objeto_comando.getNome())
-        
-mostrar_comandos()
-        
+    
+    return query
