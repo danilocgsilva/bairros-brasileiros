53d8ca5d09c861b3acfc6da15a2b8cb9da0d5328
Danilo Carlos <danilo.goes_leega@totalexpress.com.br>
2024-10-06 18:44:12 -0300

    Transferindo a responsabilidade das migrations
    Devido à falta de flexibilidade do pacote de mysql do Python, o
    trabalho de migration ficará na aplicação cli.

---
+++ cli/migrations-db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'dbname' => 'bairros_brasileiros',
+    'user' => 'root',
+    'password' => 'bairros_brasileiros',
+    'host' => 'bairros_brasileiros_banco_de_dados',
+    'driver' => 'pdo_mysql',
+];
---
+++ cli/migrations/Version20241006202116.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Danilocgsilva\BairrosBrasileirosLinhaDeComandos;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241006202116 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Cria uma tabela no banco de dados.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE locais (
+            `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            `local` VARCHAR(255),
+            `parentalidade` INT UNSIGNED UNIQUE,
+            `tipo_localidade` INT UNSIGNED UNIQUE
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;');
+        $this->addSql('ALTER TABLE locais ADD CONSTRAINT `parentalidade_id_constraint` FOREIGN KEY (`parentalidade`) REFERENCES locais (`id`);');
+        $this->addSql('CREATE TABLE tipos_locais (
+            `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+            `tipo` VARCHAR(32)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;');
+        $this->addSql('ALTER TABLE `locais` ADD CONSTRAINT `tipos_locais_tipo_localidade_constraint` FOREIGN KEY (`tipo_localidade`) REFERENCES `tipos_locais` (`id`);');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE locais DROP FOREIGN KEY tipos_locais_tipo_localidade_constraint;');
+        $this->addSql('DROP TABLE tipos_locais');
+        $this->addSql('ALTER TABLE locais DROP FOREIGN KEY parentalidade_id_constraint;');
+        $this->addSql('DROP TABLE locais;');
+    }
+}
