dd1e3f1332623f99c9f973c4c167a3936d9c7178
Danilo Carlos <contact@danilocgsilva.me>
2024-10-11 09:58:54 -0300

    Melhoria na validação do fetch

--- app/crawler/CrawlerCidadesAmapa.py
+++ app/crawler/CrawlerCidadesAmapa.py
@@ -1,23 +1,23 @@
 # from flask import request
 import requests
 from bs4 import BeautifulSoup
+import re
 
 class CrawlerCidadesAmapa:
     def buscarConteudo(self):
-        # response = requests.get("https://www.scrapingcourse.com/ecommerce/")
-        # Primeiro, tenha o html: "https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Amap%C3%A1_por_popula%C3%A7%C3%A3o"
-        # Este html contém a lista de todos os municípios do Amapá 
         response = requests.get("https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_do_Amap%C3%A1_por_popula%C3%A7%C3%A3o")
         htmlcru = response.content
         conteudo_parseado = BeautifulSoup(htmlcru, "html.parser")
-        # Depois, devo fazer o select do elemento que contém a lista 
         linhas_tabela_cidades_amapa = conteudo_parseado.select("table.wikitable.sortable tbody tr")
         lista_cidades = []
         for cidade in linhas_tabela_cidades_amapa:
             if self._eHeader(cidade):
                 continue
             ultimo_elemento_contendo_dado = cidade.select("td:nth-child(2) a")[0]
-            dado_cidade = ultimo_elemento_contendo_dado.contents[0]
+            dado_cidade_obj = ultimo_elemento_contendo_dado.contents[0]
+            dado_cidade = str(dado_cidade_obj)
+            if not self._validaDado(dado_cidade):
+                continue
             lista_cidades.append(dado_cidade)
             print(dado_cidade)
             
@@ -26,6 +26,13 @@ class CrawlerCidadesAmapa:
         if len(celulas_header) > 0:
             return True
         return False
+
+    def _validaDado(self, dado):
+        if not type(dado).__name__ == 'str':
+            return False
+        if re.search(r"<", dado):
+            return False
+        return True
             
 crawler = CrawlerCidadesAmapa()
 crawler.buscarConteudo()
