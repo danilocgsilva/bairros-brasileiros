6e1abccdedf4cda58843e55c6d035b7bb36e4da5
Danilo Carlos <contact@danilocgsilva.me>
2024-10-25 17:35:05 -0300

    Melhoria na validacao de XNext

--- app/crawler/parseadores/XNext.py
+++ app/crawler/parseadores/XNext.py
@@ -1,5 +1,10 @@
+"""
+Classe responsável por analisar a tag do ocorrência de uma linha html e retornar
+  o local, segundo regras específicas.
+"""
 from crawler.parseadores.IParseador import IParseador
 from entidades.Local import Local
+import re
 
 class XNext(IParseador):
     def __init__(self, next_num, sec_next = None):
@@ -14,5 +19,20 @@ class XNext(IParseador):
             pre_parte = str(dado_local)
             dado_local = pre_parte + dado_local.next
             
-        dado_string = str(dado_local)
-        return Local(dado_string, "cidade")
+        mensage_invalidade = self._verifica_validacao(dado_local)
+        if mensage_invalidade is not "":
+            raise Exception(mensage_invalidade)
+
+        return Local(dado_local, "cidade")
+
+    def _verifica_validacao(self, local_nome) -> bool:
+        if not type(local_nome).__name__ == "str":
+            return "Não foi possível achar uma string de nome com a receita deste parseador."
+        
+        if re.search("\n", local_nome):
+            return "Caractere de newline não é permitido."
+
+        if re.search("<", local_nome) or re.search(">", local_nome):
+            return "A receita não conseguiu navegar até a informação."
+
+        return ""
