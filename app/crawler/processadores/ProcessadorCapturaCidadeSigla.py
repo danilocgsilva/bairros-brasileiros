243a4c577c2510f0bf2b215f9be066d85cf887d1
Danilo Silva <contact@danilocgsilva.me>
2024-10-25 22:41:09 -0300

    Reapply "Salvando todas cidades brasileiras"
    This reverts commit 581ba8e7dc5203471b7f54c44f6994ba77d9778e.

--- app/crawler/processadores/ProcessadorCapturaCidadeSigla.py
+++ app/crawler/processadores/ProcessadorCapturaCidadeSigla.py
@@ -1,6 +1,7 @@
 from crawler.processadores.ProcessadorInterface import ProcessadorInterface
 from src.Dados import Dados
 from src.Repositorios.Estados import Estados
+from src.SeparaNomeCidadeSiglaEstado import SeparaNomeCidadeSiglaEstado
 
 """
 Salva uma cidade no banco de dados, em função do nome e da sigla dada entre parênteses.
@@ -14,14 +15,18 @@ class ProcessadorCapturaCidadeSigla(ProcessadorInterface):
         for estado in todos_estados_siglas:
             self.nomes_estados_pelas_siglas[estado.sigla] = estado.nome
     
-    def processar_sucesso(self, nome_local: str):
-        nome_cidade, nome_estado = self._separarCidadeEEstadoNomes()
+    def processar_sucesso(self, nome_local_com_sigla: str):
+        nome_cidade, nome_estado = self._separarCidadeEEstadoNomes(nome_local_com_sigla)
         Dados().adicionar_cidade(nome_cidade, nome_estado)
+
+    def processar_erro(self, mensagem: str):
+        print("ERRO! " + mensagem)
         
     def esta_pronto(self):
-        if self.tipo == None or self.nome == None:
-            return False
         return True
     
     def _separarCidadeEEstadoNomes(self, nome_cidade_com_sigla_estado):
-        return "", ""
+        separadorSiglaNome = SeparaNomeCidadeSiglaEstado(nome_cidade_com_sigla_estado)
+        cidade, sigla = separadorSiglaNome.separa()
+        return cidade, self.nomes_estados_pelas_siglas[sigla]
+        
